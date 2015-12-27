b00b727778a51c34ba7f7333ab98d57034878fe4
samuel <samuel.atef@coformatique.com>
2015-12-27 11:45:27 +0200

    change to realdeal
    change name of the company and changes required for test server

---
+++ site/src/main/java/com/realdeal/api/RestApiMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.realdeal.api;
+
+import org.broadleafcommerce.core.web.api.BroadleafRestApiMvcConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.realdeal.api")
+public class RestApiMvcConfiguration extends BroadleafRestApiMvcConfiguration {
+    
+}
